fbfc993567fc8f75463a8ce41af492d791edf25b
Sergio Andrés Moreno Sánchez <fineas25@gmail.com>
2026-05-12 01:00:04 -0500

    Agregar carpetas repository y service con sus respectivos archivos

---
+++ src/main/java/com/empresa/catalogo/service/ProductoService.java
@@ -0,0 +1,14 @@
+package com.empresa.catalogo.service;
+
+import com.empresa.catalogo.dto.ProductoRequestDTO;
+import com.empresa.catalogo.dto.ProductoResponseDTO;
+
+import java.util.List;
+
+// Interfaz — abstracción que cumple DIP
+public interface ProductoService {
+    ProductoResponseDTO crear(ProductoRequestDTO dto);
+    ProductoResponseDTO buscarPorId(Long id);
+    List<ProductoResponseDTO> listarActivos();
+    void eliminar(Long id);
+}
